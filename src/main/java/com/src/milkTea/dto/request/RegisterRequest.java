13d75b4bdb8bf3c0a765128b79aba592c2bbccd0
Long9904 <longvu09092004@gmail.com>
2025-04-15 16:20:12 +0700

    Add register

---
+++ src/main/java/com/src/milkTea/dto/request/RegisterRequest.java
@@ -0,0 +1,23 @@
+package com.src.milkTea.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+
+    @Schema(description = "Email", example = "registerEmail@gmail.com")
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Schema(description = "Full name of the user", example = "Long Cha")
+    @Size(min = 1, max = 30, message = "Full name must be between 1 and 30 characters")
+    private String fullName;
+
+    @Schema(description = "Phone number", example = "0841234567")
+    @Pattern(regexp = "(84|0[35789])[0-9]{8}", message = "Invalid phone number")
+    private String phoneNumber;
+}
