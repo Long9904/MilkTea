a9a9a965c0d45149ad84f57ce58923616e52a23f
Long9904 <longvu09092004@gmail.com>
2025-04-15 10:48:46 +0700

    Empty code

---
+++ src/main/java/com/src/milkTea/MilkTeaApplication.java
@@ -0,0 +1,13 @@
+package com.src.milkTea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilkTeaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilkTeaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/src/milkTea/MilkTeaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.milkTea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MilkTeaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
