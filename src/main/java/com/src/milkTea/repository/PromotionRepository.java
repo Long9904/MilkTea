42565c059fefb64ec97fe87c58f6a47ee8c0fb95
Long9904 <longvu09092004@gmail.com>
2025-05-09 12:27:46 +0700

    Add code for promotion

--- src/main/java/com/src/milkTea/repository/PromotionRepository.java
+++ src/main/java/com/src/milkTea/repository/PromotionRepository.java
@@ -3,6 +3,12 @@ package com.src.milkTea.repository;
 import com.src.milkTea.entities.Promotion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+
+    boolean existsByCode(String code);
+
+    boolean existsByCodeAndIdNot(String code, Long id);
 
 }
